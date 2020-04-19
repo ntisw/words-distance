f4eeaa0e2f9935eae72205d811cf8e4ad831f532
Nantipat <scemisnis@gmail.com>
2020-04-19 14:59:37 +0700

    added find_word_distance function

--- word-distance.py
+++ word-distance.py
@@ -1,5 +1,7 @@
 import csv
-def transform_array(filename,place_name):
+
+
+def transform_array(filename, place_name):
     allcomment_in_place = []
     with open('./data/'+filename+'.csv', mode='r', encoding='utf-8') as csv_file:
         csv_reader = csv.DictReader(csv_file)
@@ -17,14 +19,26 @@ def transform_array(filename,place_name):
 
     return allcomment_in_place
 
-def find_word_distance(comments,noun_and_adj = True ,noun_and_verb = True , noun_and_adv =  True ) :
+
+def find_word_distance(comments, noun_and_adj=True, noun_and_verb=True, noun_and_adv=True):
     words_distance = []
-    for comment in comments :
-        for sentence in comment :
-            for word in sentence:
-                print (word)
+    for comment in comments:
+        for sentence in comment:
+            i = 0
+            while i < len(sentence):
+
+                if sentence[i].find("NOUN") != -1:
+                    j = i+1
+                    while j < len(sentence) :
+                        if (sentence[j].find("ADJ") != -1 and noun_and_adj) \
+                                or (sentence[j].find("VERB") != -1 and noun_and_verb) \
+                                or (sentence[j].find("ADV") != -1 and noun_and_adv):
+
+                            print(f"{sentence[i]} and {sentence[j]} with {j-(i+1)}")
+
+                        j += 1
+                i += 1
 
 
-
-comments = transform_array("google","patong")
+comments = transform_array("google", "patong")
 find_word_distance(comments)
